bbbd4f24806a02098be79a975f30d9a5dfd7949b
leo davis <leob7v@gmail.com>
2023-06-10 14:34:54 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declaring and initialize variables
+string name = "John";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 95;
+decimal currentPrice = 15.99M;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+Console.WriteLine($"My dog's name is {name}, He is {age} years old");
